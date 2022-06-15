44a19996b0807bb71105b80cc4e57b6a191a04a0
unknown <edoafrian90@gmail.com>
2022-06-15 23:46:27 +0700

    fixed

--- src/pages/TodoPages.js
+++ src/pages/TodoPages.js
@@ -1,37 +1,42 @@
 import React from "react"
 import {
-  FormControl,
-  Button
+  Button, FormControl, InputGroup, Table
 } from 'react-bootstrap'
 import Axios from 'axios'
 import './style.css'
 import { back } from '../Asset/index'
 
-const API = 'http://localhost:2000/activities/'
+const API = 'https://api.coinpaprika.com/v1/coins/'
 
 class TodoPages extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      list: []
+      list: [],
+      page: 1,
+      prodPerPage: 4,
+      max: 0
     }
   }
-  // UNSAFE_componentWillMount() {
 
-  //   alert('ini will mount')
-  // }
 
   componentDidMount() {
     Axios.get(`${API}`)
       .then(res => {
         console.log(res.data)
-        this.setState({ list: res.data })
+        this.setState({ list: res.data, max: Math.ceil(res.data.length / this.state.prodPerPage) })
       })
       .catch(error => {
         console.log(error + 'ini eror get list redzone');
       });
   }
+  onNextPage = () => {
+    this.setState({ page: this.state.page + 1 })
 
+  }
+  onPrevPage = () => {
+    this.setState({ page: this.state.page - 1 })
+  }
   onDelete = (id) => {
     Axios.delete(`${API}${id}`)
       .then(res => {
@@ -41,74 +46,74 @@ class TodoPages extends React.Component {
           })
       })
   }
-
-  onComplete = (id) => {
-    Axios.patch(`${API}${id}`, { isCompleted: true })
-      .then(res => {
-        Axios.get(`${API}`)
-          .then(res => {
-            this.setState({ list: res.data })
-          })
-      })
-  }
-
   showData = () => {
+    let beginningIndex = (this.state.page - 1) * this.state.prodPerPage
+    let currentList = this.state.list.slice(beginningIndex, beginningIndex + this.state.prodPerPage)
     return (
-      this.state.list.map((item, index) => {
-        return (
-          <div className="contain">
-            <p> {index + 1}. {item.name}</p>
-            <div>
-              <Button variant="success" onClick={() => this.onDelete(item.id)} className="me-2 btn">Delete</Button>
-              <Button variant="primary" onClick={() => this.onComplete(item.id)} className="btn"
-                disabled={item.isCompleted}
-              >
-                {item.isCompleted ? "Finished" : "Completed"}
-              </Button>
-            </div>
-          </div>
-        )
-      })
-
+      <Table>
+        <thead style={{ backgroundColor: '#3783c6' }}>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>Symbol</th>
+            <th>Rank</th>
+            <th>Type</th>
+            <th>Active</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody className="contain">
+          {currentList.map(item => {
+            return (
+              <tr>
+                <td style={{ color: 'blue' }}>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.symbol}</td>
+                <td>{item.rank}</td>
+                <td>{item.type}</td>
+                <td>{item.is_active}</td>
+                <td><div>
+                  <Button variant="danger" onClick={() => this.onDelete(item.id)} className="me-2 btn">Delete</Button>
+                </div></td>
+              </tr>
+            )
+          })
+          }
+        </tbody>
+      </Table>
     )
   }
-
-  onAdd = () => {
-    let newTodo = this.refs.todo.value
-
-    let obj = {
-      name: newTodo,
-      isCompleted: false
-    }
-    Axios.post(`${API}`, obj)
-      .then(res => {
-        Axios.get(`${API}`)
-          .then(res => {
-            this.setState({ list: res.data })
-          })
-      })
-    this.refs.todo.value = ''
-  }
-
-
   render() {
-    // alert('ini render')
-    console.log(this.state.list);
     return (
       <div className="layer-page">
-        <div className="nav"> <h2 style={{ paddingLeft: '2rem',paddingTop:'0.5rem', color:'white' }}>TO DO LIST APP</h2> <h3 style={{ paddingRight: '2rem',paddingTop:'0.5rem',color:'white' }}>You have {this.state.list.length} to do items</h3></div>
+        <p className="coin">Coin List</p>
+        <div className="search">
+        <FormControl className="formControl"
+            placeholder="select"
+          />
+          <InputGroup className="mb-3">
+          <FormControl className="formControl1"
+            placeholder="search"
+          />
+          <Button className="butn">search</Button>
+        </InputGroup>
+        </div>
         <div className="page">
-          <div className="list"><h1>To Do List</h1>
+          <div className="list">
             {this.showData()}
-            <div className="input">
-              <FormControl
-                placeholder="Input New Todo"
-                ref="todo"
-              />
-              <Button variant="info" onClick={this.onAdd} classname="ml-2">Add</Button>
+            <div className='pagination'>
+              <Button
+                disabled={this.state.page <= 1 ? true : false}
+                onClick={this.onPrevPage}
+                variant="light"><i class="fad fa-angle-double-left"></i></Button>
+              <p style={{ marginBottom: '0px' }}>page {this.state.page} of {this.state.max}</p>
+              <Button
+                disabled={this.state.page >= this.state.max ? true : false}
+                onClick={this.onNextPage}
+                variant="light"><i class="fad fa-angle-double-right"></i></Button>
             </div>
           </div>
-          <div><img src={back} /></div>
+          {/* <div><img src={back} /></div> */}
         </div>
 
       </div>
